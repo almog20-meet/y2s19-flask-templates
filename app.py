931c7700bc388eec4357f388c65990db50ed9222
myname21-meet <myname21@meet.mit.edu>
2019-07-29 11:26:54 -0400

    labs

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home_page():
-    return "Hello World"
-
+    return render_template ("index.html" , foods = ["Pasta" , "Pizza" , "Hamburger" , "Sushi"])
+    ham = "https://www.google.co.il/url?sa=i&source=images&cd=&ved=2ahUKEwi3gcqlt9rjAhXFaFAKHWLzC-MQjRx6BAgBEAU&url=https%3A%2F%2Fwww.epicurious.com%2Frecipes%2Ffood%2Fviews%2Fthe-ultimate-hamburger-232191&psig=AOvVaw2SMOEl6QPSV5l1LqSwJfvs&ust=1564500096544029"
 if __name__ == '__main__':
    app.run(debug = True)
